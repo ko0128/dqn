40a6737e1d214df2c741541c5400f4e3ca03ab1c
ko0128 <89089017+ko0128@users.noreply.github.com>
2023-07-01 20:52:09 +0800

    try to run every code

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     # env = gym.make(ENV_NAME)
     env = WareHouse(grid_data, 2)
 
-    FIX_RANDOM_SEED =True
+    FIX_RANDOM_SEED =False
     if FIX_RANDOM_SEED:
         torch.manual_seed(420)
         if torch.cuda.is_available():
