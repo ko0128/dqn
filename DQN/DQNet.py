9a693b6687f5930aef68cbd023f57b9e0e410790
ko0128 <89089017+ko0128@users.noreply.github.com>
2023-07-01 21:29:24 +0800

    try to run every code

--- DQN/DQNet.py
+++ DQN/DQNet.py
@@ -9,30 +9,39 @@ import torchvision.models as models
 class DQN(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_layers: int = 64):
         super(DQN, self).__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_layers)
-        self.fc2 = nn.Linear(hidden_layers, hidden_layers // 2)
-        self.fc3 = nn.Linear(hidden_layers // 2, output_dim)
+        # self.fc1 = nn.Linear(input_dim, hidden_layers)
+        # self.fc2 = nn.Linear(hidden_layers, hidden_layers // 2)
+        # self.fc3 = nn.Linear(hidden_layers // 2, output_dim)
         # self.vgg_model = models.vgg11(pretrained=False)
 
-        # self.cnn = nn.Sequential(
-        #     nn.Conv2d(4, 16, 3),
-        #     nn.ReLU(),
-        #     nn.Conv2d(16, 8, 5),
-        #     nn.ReLU(),
-        # )
-        # self.pred = nn.Sequential(
-        #     nn.Linear(8*4*4, 32),
-        #     nn.ReLU(),
-        #     nn.Linear(32, 4)
-        # )
+        self.cnn = nn.Sequential(
+            nn.Conv2d(4, 32, 3, padding = 1),
+            nn.Conv2d(32, 32,3, padding = 1),
+            nn.Conv2d(32, 32,3, padding = 1),
+            nn.MaxPool2d(2)
+        )
+
+        self.cnn2 = nn.Sequential(
+            nn.Conv2d(32, 32,3, padding = 1),
+            nn.Conv2d(32, 32,3, padding = 1),
+            nn.Conv2d(32, 32,3, padding = 1),
+            nn.MaxPool2d(2)
+        )
+        self.pred = nn.Sequential(
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 4),
+        )
 
     def forward(self, x):
-        x = x.view(x.size()[0], -1)
-        # x = x.view(-1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
-        # x = self.cnn(x)
-        # x = x.view(x.shape[0], -1)
-        # x = self.pred(x)
-        # return x
+        # x = x.view(x.size()[0], -1)
+        # x = F.relu(self.fc1(x))
+        # x = F.relu(self.fc2(x))
+        # return self.fc3(x)
+        x = self.cnn(x)
+        x = self.cnn2(x)
+        x = x.view(x.shape[0], -1)
+        x = self.pred(x)
+        return x
