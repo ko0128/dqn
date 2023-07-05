820a6a99ea04d35bfc09b5dd8c5475b03c5e6892
ko0128 <89089017+ko0128@users.noreply.github.com>
2023-07-05 16:05:11 +0800

    commented reward

--- DQN/DQNAgent.py
+++ DQN/DQNAgent.py
@@ -137,7 +137,8 @@ class DQNAgent:
         # print(f'batch_states.shape: {batch_actions.shape}')
         # current_qvals = self.policy_net.forward(batch_states).gather(dim=1, index=batch_actions.unsqueeze(1))
         current_qvals = self.policy_net.forward(batch_states).gather(dim=1, index=batch_actions.unsqueeze(1)).cpu()
-
+        print(current_qvals.shape)
+        
         # compute target q-values
         # target_qvals = self.target_net.forward(batch_next_states).max(1).values.detach()
         target_qvals = self.target_net.forward(batch_next_states).cpu().max(1).values.detach()
